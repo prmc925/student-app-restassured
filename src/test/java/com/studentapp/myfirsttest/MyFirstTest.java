93d6453e33ea926b906e9ddce52c389353b8eaa0
= <=>
2025-05-23 15:18:13 +0100

    Skeleton of student-app-restassured

---
+++ src/test/java/com/studentapp/myfirsttest/MyFirstTest.java
@@ -0,0 +1,15 @@
+package com.studentapp.myfirsttest;
+
+import com.studentapp.testbase.TestBase;
+import org.testng.annotations.Test;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class MyFirstTest extends TestBase {
+
+    @Test
+    public void getAllStudentInfo() {
+
+    }
+}
