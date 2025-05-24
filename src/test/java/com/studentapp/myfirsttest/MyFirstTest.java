68fd6b190969c9f5c7966af417c13c2f0387aac6
= <=>
2025-05-24 17:38:51 +0100

    Today's class update

--- src/test/java/com/studentapp/myfirsttest/MyFirstTest.java
+++ src/test/java/com/studentapp/myfirsttest/MyFirstTest.java
@@ -1,7 +1,10 @@
 package com.studentapp.myfirsttest;
 
 import com.studentapp.testbase.TestBase;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
 
 /**
  * Created by Jay Vaghani
@@ -10,6 +13,16 @@ public class MyFirstTest extends TestBase {
 
     @Test
     public void getAllStudentInfo() {
+        /*given()
+                .when()
+                .get("http://localhost:8081/student/list")
+                .then()
+                .statusCode(200);*/
 
+        Response response = given()
+                .when()
+                .get("/list");
+        response.then().statusCode(200);
+        response.prettyPrint();
     }
 }
--- src/test/java/com/studentapp/studentinfo/StudentPostTest.java
+++ src/test/java/com/studentapp/studentinfo/StudentPostTest.java
@@ -1,7 +1,15 @@
 package com.studentapp.studentinfo;
 
+import com.studentapp.model.StudentPojo;
 import com.studentapp.testbase.TestBase;
+import com.studentapp.utils.TestUtils;
+import io.restassured.response.Response;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
 
 /*
  *  Created by Jay
@@ -11,5 +19,25 @@ public class StudentPostTest extends TestBase {
     @Test
     public void createStudent() {
 
+        String email = TestUtils.getRandomValue() + "prime@gmail.com";
+
+        List<String> courseList = new ArrayList<>();
+        courseList.add("Automation");
+        courseList.add("Java");
+
+        StudentPojo studentPojo = new StudentPojo();
+        studentPojo.setFirstName("Jay");
+        studentPojo.setLastName("Vaghani");
+        studentPojo.setEmail(email);
+        studentPojo.setProgramme("Automation Tester");
+        studentPojo.setCourses(courseList);
+
+        Response response = given()
+                .header("Content-Type", "application/json")
+                .when()
+                .body(studentPojo)
+                .post();
+        response.prettyPrint();
+        response.then().statusCode(201);
     }
 }
